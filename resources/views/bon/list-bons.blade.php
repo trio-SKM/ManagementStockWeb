239d31113a02c84818e21e25fd6d21f790e1c717
Mohamed YOUSFI <mr.mohamed.yousfi@gmail.com>
2022-09-17 00:35:02 +0100

    Integration list bon et list bons - Résoudre un problème dans list produit

--- resources/views/bon/list-bons.blade.php
+++ resources/views/bon/list-bons.blade.php
@@ -1,54 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>All bons</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
-</head>
+@section('title', 'List de Bons de Commandes')
 
-<body>
-    @if (session('status'))
-        {{ session('status', '') }}
-    @endif
-    <div>
-        <a href="{{ route('bon_commande.create') }}">Ajouter un bon de commande</a>
-    </div>
-    @if (count($bons) > 0)
-        <table border="1" style="margin-bottom: 30px">
-            <thead>
-                <th>N°</th>
-                <th>Num bon de commande</th>
-                <th>Nom de fournisseur</th>
-                <th colspan="4">actions</th>
-            </thead>
-            <tbody>
-                @foreach ($bons as $bon)
-                    <tr>
-                        <td>{{ $loop->iteration }}</td>
-                        <td>{{ $bon->num }}</td>
-                        <td>{{($bon->fournisseur != null)?$bon->fournisseur->nom_complet: "- - -"}}</td>
-                        <td><a href="{{ route('bon_commande.show', ['bon_commande' => $bon->id]) }}">détails</a></td>
-                        <td><a href="{{ route('bon_commande.edit', ['bon_commande' => $bon->id]) }}">modifier</a></td>
-                        <td>
-                            <form action="{{ route('bon_commande.destroy', ['bon_commande' => $bon->id]) }}" method="post">
-                                @csrf
-                                @method('DELETE')
-                                <input type="submit" value="supprimer">
-                            </form>
-                        </td>
-                        <td>
-                            <button class="btn_show_produits" data-bon_commande_id="{{ $bon->id }}">Afficher ses produits</button>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-        <table border="1">
-            <thead>
+@section('content_page')
+<div class="modal fade gd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+        <table id="table-details-bon" class="table mb-0">
+            <thead class="table-light">
                 <th>N°</th>
                 <th>REF</th>
                 <th>Libelle</th>
@@ -61,12 +20,92 @@
 
             </tbody>
         </table>
-    @else
-        <div>
-            <p>Il y a aucun bon ce moment.</p>
+    </div>
+  </div>
+</div>
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-12">
+            <!-- Page header -->
+            <div class="border-bottom pb-4 mb-4">
+                <h3 class="mb-0 fw-bold">List des Fournisseurs</h3>
+            </div>
         </div>
-    @endif
-    <script src="{{ asset('js/bon/list-bons.js') }}"></script>
-</body>
+    </div>
 
-</html>
+    <div class="py-4">
+        <div class="card h-100">
+            <!-- card header  -->
+            <div class="card-header bg-white py-3 text-end">
+                <h4 class="mb-0"><a class="btn btn-dark" href="{{ route('bon_commande.create') }}"><i class="bi bi-plus"></i>
+                        Ajouter un bon de commande</a> </h4>
+            </div>
+            <!-- table  -->
+            @if (count($bons) > 0)
+                <div class="table-responsive">
+                    <table class="table text-nowrap mb-0">
+                        <thead class="table-light">
+                            <tr>
+                                <th>N°</th>
+                                <th>Num bon de commande</th>
+                                <th>Nom de fournisseur</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($bons as $bon)
+                                <tr>
+                                    <td class="align-middle">{{ $loop->iteration }}</td>
+                                    <td class="align-middle">{{ $bon->num }}</td>
+                                    <td class="align-middle">{{ ($bon->fournisseur != null)?$bon->fournisseur->nom_complet: "- - -" }}</td>
+                                    <td class="align-middle">
+                            <button type="button" data-bs-toggle="modal" data-bs-target=".gd-example-modal-lg" class="btn btn-primary btn_show_produits" data-bon_commande_id="{{ $bon->id }}"><i class="bi bi-eye"></i></button>
+                        </td>
+                                    <td class="align-middle">
+                                        <div class="dropdown dropstart">
+                                            <a class="text-muted text-primary-hover" href="#" role="button"
+                                                id="dropdownTeamOne" data-bs-toggle="dropdown" aria-haspopup="true"
+                                                aria-expanded="false">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                    viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                    class="feather feather-more-vertical icon-xxs">
+                                                    <circle cx="12" cy="12" r="1"></circle>
+                                                    <circle cx="12" cy="5" r="1"></circle>
+                                                    <circle cx="12" cy="19" r="1"></circle>
+                                                </svg>
+                                            </a>
+                                            <div class="dropdown-menu" aria-labelledby="dropdownTeamOne">
+                                                <a class="dropdown-item"
+                                                    href="{{ route('bon_commande.edit', ['bon_commande' => $bon->id]) }}">Modifier</a>
+                                                <form
+                                                    action="{{ route('bon_commande.destroy', ['bon_commande' => $bon->id]) }}"
+                                                    method="post">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="dropdown-item">Supprimer</button>
+                                                </form>
+                                                <a class="dropdown-item"
+                                                    href="{{ route('bon_commande.show', ['bon_commande' => $bon->id]) }}">Détails</a>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            @else
+                <x-data-not-found message="Il y a aucun bon ce moment." />
+            @endif
+            @if (session()->exists('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status', '') }}
+                </div>
+            @endif
+        </div>
+    </div>
+@endsection
+@section('custom_script')
+<script src="{{ asset('js/bon/list-bons.js') }}"></script>
+@endsection
--- resources/views/produit/list-produit.blade.php
+++ resources/views/produit/list-produit.blade.php
@@ -18,13 +18,13 @@
                     <div class="d-flex align-items-center
                     justify-content-between">
                         <div>
-                            <h4 class="mb-0">Produit: </h4>
+                            <h4 class="mb-0">Produit: {{ $produit->libelle }}</h4>
                         </div>
                     </div>
-                    <div class="mt-3">
-                        {{-- <img src="../assets/images/placeholder/placeholder-4by3.svg" class="card-img-top mb-2"
-                            alt="..."> --}}
-                        <h4 class="card-title">{{ $produit->libelle }}</h4>
+                   {{--  <div class="mt-3">
+                        <img src="../assets/images/placeholder/placeholder-4by3.svg" class="card-img-top mb-2"
+                            alt="..."> 
+                        <h4 class="card-title"></h4>
                         {{-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
                             card's content.</p>
                         <h5 class="">Les caractèristique de produit</h5>
@@ -32,31 +32,21 @@
                             <li class="list-group-item">Cras justo odio</li>
                             <li class="list-group-item">Dapibus ac facilisis in</li>
                             <li class="list-group-item">Vestibulum at eros</li>
-                        </ul> --}}
-                    </div>
+                        </ul> 
+                    </div>--}}
                     <div class="d-flex align-items-center justify-content-around">
                         <div class="text-center">
                             <h1 class="mt-3  mb-1 fw-bold">{{ $produit->price }}</h1>
                             <p>Prix d'achat</p>
                         </div>
                         <div class="text-center">
-                            <h1 class="mt-3  mb-1 fw-bold">{{ $produit->price_buy }}</h1>
-                            <p>Prix d'achat</p>
+                            <h1 class="mt-3  mb-1 fw-bold">{{ $produit->price_buy }}152</h1>
+                            <p>Prix de vente</p>
                         </div>
                         <div class="text-center">
                             <h1 class="mt-3  mb-1 fw-bold">{{ $produit->qte }}</h1>
                             <p>Quantité</p>
                         </div>
-                        @if ($produit->bon_commande != null)
-                            <div class="text-center">
-                                <h1 class="mt-3  mb-1 fw-bold">{{ $produit->bon_commande->num }}</h1>
-                                <p>Bon de commande</p>
-                            </div>
-                            <div class="text-center">
-                                <h1 class="mt-3  mb-1 fw-bold">{{ $produit->bon_commande->fournisseur->nom_complet }}</h1>
-                                <p>Fournisseur</p>
-                            </div>
-                        @endif
                     </div>
                     <div class="d-grid gap-2">
                         <a href="{{ route('produit.edit', ['produit' => $produit->id]) }}"
@@ -80,27 +70,22 @@
                     <table class="table text-nowrap">
                         <thead class="table-light">
                             <tr>
-                                <th>Name</th>
-                                <th>Role</th>
-                                <th>Last Activity</th>
+                                <th>Fournisseur</th>
+                                <th>Bon de Commande</th>
+                                <th>Prix d'achat</th>
+                                <th>Prix de vente</th>
+                                <th>Date de transaction</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
-                                <td class="align-middle">
-                                    <div class="d-flex align-items-center">
-                                        <div>
-                                            <img src="../assets/images/avatar/avatar-2.jpg" alt=""
-                                                class="avatar-md avatar rounded-circle">
-                                        </div>
-                                        <div class="ms-3 lh-1">
-                                            <h5 class=" mb-1">Anita Parmar</h5>
-                                            <p class="mb-0">anita@example.com</p>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td class="align-middle">Front End Developer</td>
-                                <td class="align-middle">3 May, 2021</td>
+                                @if ($produit->bon_commande != null)
+                                    <td class="align-middle">{{ $produit->bon_commande->num }}</td>
+                                    <td class="align-middle">{{ $produit->bon_commande->fournisseur->nom_complet }}</td>
+                                @endif
+                                <td class="align-middle">{{ $produit->price }}</td>
+                                <td class="align-middle">{{ $produit->price_buy }}</td>
+                                <td class="align-middle">23/12/2020</td>
                             </tr>
                         </tbody>
                     </table>
